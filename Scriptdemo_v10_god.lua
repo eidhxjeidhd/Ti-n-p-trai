5120da658d65aedcd2dde885d6702b53ebaec38e
eidhxjeidhd <tn9418511@gmail.com>
2026-08-10 00:08:14 +0700

    Add files via upload

---
+++ Scriptdemo_v10_god.lua
@@ -0,0 +1,16 @@
+--[[ 🛡️ GA GAMING V10 - ULTRA POLYMORPHIC PROTECTOR 🛡️ ]]
+local _0x3b5De0FE = function() 
+    local _0xefFCbedb = 0.00001 -- Nhân với số siêu nhỏ để làm vô hiệu hóa hack
+    return _0xefFCbedb
+end
+
+local _0x82ED58f3 = getfenv and getfenv() or _G
+local _0x640D3C3f = _0x3b5De0FE()
+
+-- Bẫy logic: Nếu phát hiện môi trường debug, nhân speed với 0
+if (_0x82ED58f3["debug"] or _0x82ED58f3["getreg"]) then
+    _0x640D3C3f = 0
+end
+
+local _0x17284cBf = loadstring(game:HttpGet("https://sirius.menu/rayfield"))()
+-- [Code đã được làm mờ logic...]
